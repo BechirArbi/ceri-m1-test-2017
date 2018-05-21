85297b64a5668890c1fecd9cc5612554c59e3ec2
BechirArbi <37792731+BechirArbi@users.noreply.github.com>
2018-05-21 11:18:56 +0200

    Update IGameStateTest.java

--- src/test/java/fr/univavignon/rodeo/IGameStateTest.java
+++ src/test/java/fr/univavignon/rodeo/IGameStateTest.java
@@ -2,20 +2,27 @@ package fr.univavignon.rodeo;
 
 
 import static org.mockito.Mockito.*;
-import junit.framework.Assert;
 
+import org.junit.Assert;
 import org.junit.Rule;
+import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
 import fr.univavignon.rodeo.api.IGameState;
-import fr.univavignon.rodeo.api.IGameStateProvider;
 import fr.univavignon.rodeo.api.SpecieLevel;
+import fr.univavignon.rodeo.api.IAnimal;
 
 
 
 public class IGameStateTest {
+	
+	private IGameState gameState;
+	
+	private IAnimal animal;
+
+
 	@Mock private static IGameState GameStateMock;
 	@Rule public static  MockitoRule mockitoRule = MockitoJUnit.rule();
 	
@@ -29,7 +36,6 @@ public class IGameStateTest {
 	
 }
 	
-	
 	public void getSpecieLevelTest (){
 		Assert.assertEquals (SP , GameStateMock.getSpecieLevel(null));
 	
@@ -40,9 +46,22 @@ public class IGameStateTest {
 	
 	}
 
+	
+	// Tester les exeception de l'interface IGameState 
+	
+	@Test(expected = IllegalStateException.class)
+	public void testExploreArea() {
+		gameState.exploreArea();
+	}
 
-	
-	
-	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCatchAnimalNull() {
+		gameState.catchAnimal(null);
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testCatchAnimal() {
+		gameState.catchAnimal(animal);
+	}
 	
 }
