1c253253b70f7751b6d0d21bef4aea14ef7a884d
BechirArbi <37792731+BechirArbi@users.noreply.github.com>
2018-05-21 11:13:41 +0200

    Update GameStateProvider.java

--- src/main/java/fr/univavignon/rodeo/imp/GameStateProvider.java
+++ src/main/java/fr/univavignon/rodeo/imp/GameStateProvider.java
@@ -1,5 +1,37 @@
 package fr.univavignon.rodeo.imp;
 
-public class GameStateProvider {
+
+import java.util.ArrayList;
+
+import fr.univavignon.rodeo.api.IGameState;
+import fr.univavignon.rodeo.api.IGameStateProvider;
+
+public class GameStateProvider implements IGameStateProvider {
+
+	@SuppressWarnings("unused")
+	private IGameState gameState;
+
+	
+	private ArrayList<IGameState> iGS;
+	
+	
+	public void save(IGameState gameState) {
+		
+	}
+
+	
+	public IGameState get(String name) throws IllegalArgumentException {
+		if (name == null) throw new IllegalArgumentException();
+
+		for (IGameState iGss : iGS)
+			if (iGss.getName().equals(name))
+				return iGss;
+		
+		IGameState var = new GameState("var_name");
+		iGS.add(var);
+		return var;
+		
+	
+	}
 
 }
