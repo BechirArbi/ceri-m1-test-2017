7de24ca6e2278490c81fcb1802324a1e78c4f12b
BechirArbi <37792731+BechirArbi@users.noreply.github.com>
2018-05-21 11:13:18 +0200

    Update GameState.java

--- src/main/java/fr/univavignon/rodeo/imp/GameState.java
+++ src/main/java/fr/univavignon/rodeo/imp/GameState.java
@@ -1,5 +1,63 @@
 package fr.univavignon.rodeo.imp;
 
-public class GameState {
+
+import fr.univavignon.rodeo.api.IAnimal;
+import fr.univavignon.rodeo.api.IEnvironmentProvider;
+import fr.univavignon.rodeo.api.IGameState;
+import fr.univavignon.rodeo.api.ISpecie;
+import fr.univavignon.rodeo.api.SpecieLevel;
+
+import fr.univavignon.rodeo.imp.EnvironmentProvider;
+
+
+
+public class GameState implements IGameState {
+
+	private String name;
+
+
+	private int currentArea;
+    private String currentEnvironment = null;
+	private IEnvironmentProvider environmentProvider;
+
+	public GameState(final String name) {
+		this.name = name;
+		environmentProvider = new EnvironmentProvider();
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+	
+
+	@Override
+	public void exploreArea() throws IllegalStateException {
+		currentArea++;
+		
+		if (currentArea > environmentProvider.getEnvironment(currentEnvironment).getAreas()) {
+			
+			currentArea = 0;
+		}
+	}
+
+
+	@Override
+	public void catchAnimal(IAnimal animal) throws IllegalArgumentException,
+		IllegalStateException {
+
+	}
+	@Override
+	public SpecieLevel getSpecieLevel(ISpecie specie) 	throws IllegalArgumentException {
+		return null;
+	}
+
+	@Override
+	public int getProgression() {
+
+		return 100;
+	}
+
+
 
 }
