910c3e4840d3ebff6f037aea9dc170f3d9a608b7
BechirArbi <37792731+BechirArbi@users.noreply.github.com>
2018-05-21 11:11:13 +0200

    Update Animal.java

--- src/main/java/fr/univavignon/rodeo/imp/Animal.java
+++ src/main/java/fr/univavignon/rodeo/imp/Animal.java
@@ -1,20 +1,51 @@
 package fr.univavignon.rodeo.imp;
 
-import org.junit.Before;
+import fr.univavignon.rodeo.api.IAnimal;
 
-public class Animal {
 
-	
-	@Before
-	public void setUp() throws Exception {
+public class Animal implements IAnimal {
+
+	private String name;
+	private int XP;
+	private boolean Secret;
+	private boolean Endangered;
+	private boolean Boss;
+
+	public Animal(final String name, final int XP, final boolean Secret,
+	              final boolean Endangered, final boolean Boss) {
+		this.name = name;
+		this.XP = XP;
+		this.Secret = Secret;
+		this.Endangered = Endangered;
+		this.Boss = Boss;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public int getXP() {
+		return XP;
+	}
+
+	@Override
+	public boolean isSecret() {
+		return Secret;
+	}
+
+	@Override
+	public boolean isEndangered() {
+		return Endangered;
+	}
+
+	@Override
+	public boolean isBoss() {
+		return Boss;
+	}
+
 
 }
-	
-	
-	
-	
-	
-	
-	
-	
-}
+
+
