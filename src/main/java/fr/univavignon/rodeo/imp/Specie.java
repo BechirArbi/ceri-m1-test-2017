220828719021408c3f14ba8f86d29d10203aa9be
BechirArbi <37792731+BechirArbi@users.noreply.github.com>
2018-05-21 11:14:06 +0200

    Update Specie.java

--- src/main/java/fr/univavignon/rodeo/imp/Specie.java
+++ src/main/java/fr/univavignon/rodeo/imp/Specie.java
@@ -1,5 +1,35 @@
 package fr.univavignon.rodeo.imp;
 
-public class Specie {
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.univavignon.rodeo.api.IAnimal;
+import fr.univavignon.rodeo.api.ISpecie;
+
+public class Specie implements ISpecie {
+
+	private List<IAnimal> animals;
+	private int area;
+
+	public Specie(int var_area,List<IAnimal> animals) {
+		super();
+		area = var_area;
+		animals =new ArrayList<IAnimal>();
+		animals.addAll(animals);
+	}
+
+	public int getArea() {
+		return area;
+	}
+
+	public List<IAnimal> getAnimals() {
+		return animals;
+	}
+
+	@Override
+	public String getName() {
+		return null;
+	}
 
 }
