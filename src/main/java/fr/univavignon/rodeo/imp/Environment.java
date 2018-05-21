7f42db1b4b825d9d9a26f3b11b44c104ad35baba
BechirArbi <37792731+BechirArbi@users.noreply.github.com>
2018-05-21 11:11:58 +0200

    Update Environment.java

--- src/main/java/fr/univavignon/rodeo/imp/Environment.java
+++ src/main/java/fr/univavignon/rodeo/imp/Environment.java
@@ -1,5 +1,39 @@
 package fr.univavignon.rodeo.imp;
 
-public class Environment {
 
-}
+import java.util.List;
+
+import fr.univavignon.rodeo.api.IEnvironment;
+import fr.univavignon.rodeo.api.ISpecie;
+
+	public class Environment implements IEnvironment {
+
+		private String name;
+		private int areas;
+		private List<ISpecie> species;
+
+		public Environment(final String name, final int areas, final List<ISpecie> species) {
+			this.name = name;
+			this.areas = areas;
+			this.species = species;
+		}
+
+		@Override
+		public String getName() {
+			return name;
+		}
+
+		@Override
+		public int getAreas() {
+			return areas;
+		}
+
+		@Override
+		public List<ISpecie> getSpecies() {
+			return species;
+		}
+
+	}
+	
+	
+
