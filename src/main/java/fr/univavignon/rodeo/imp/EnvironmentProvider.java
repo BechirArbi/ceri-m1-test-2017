0e570068ffe5af7285ba5ebe7b031dcbb45b25a7
BechirArbi <37792731+BechirArbi@users.noreply.github.com>
2018-05-21 11:12:47 +0200

    Update EnvironmentProvider.java

--- src/main/java/fr/univavignon/rodeo/imp/EnvironmentProvider.java
+++ src/main/java/fr/univavignon/rodeo/imp/EnvironmentProvider.java
@@ -1,5 +1,30 @@
 package fr.univavignon.rodeo.imp;
 
-public class EnvironmentProvider {
+
+import java.util.List;
+import java.util.Map;
+
+import fr.univavignon.rodeo.api.IEnvironment;
+import fr.univavignon.rodeo.api.IEnvironmentProvider;
+
+public class EnvironmentProvider implements IEnvironmentProvider {
+
+	private Map<String, Environment> environments;
+
+	public EnvironmentProvider() {
+			}
+
+
+	@SuppressWarnings("unchecked")
+	public List<String> getAvailableEnvironments() {
+		return (List<String>) environments.keySet();
+	}
+
+	public IEnvironment getEnvironment(String name)
+	throws IllegalArgumentException {
+		if (!environments.containsKey(name))
+			throw new IllegalArgumentException("AAA");
+		return environments.get(name);
+	}
 
 }
